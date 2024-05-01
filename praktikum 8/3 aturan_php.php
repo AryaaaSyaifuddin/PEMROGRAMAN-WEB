553a9811c7bad578385fb56098040b4351fef466
AryaSyaifuddin02 <114994983+AryaaaSyaifuddin@users.noreply.github.com>
2024-05-01 20:23:02 +0700

    Add files via upload

---
+++ praktikum 8/3 aturan_php.php
@@ -0,0 +1,26 @@
+<?php
+    // Standard
+    echo "<p>Kalimat ini berasal dari Standard PHP tag </p>";
+    
+    // Case Insensitive
+    echo "<p>hallo ges</p>";
+    ECHO "<p>HALLO GES</p>";
+    EcHo "<p>HeLlO gEs</p>";
+
+    // Case Insensitive Error / Warning
+    $arya = "arya";
+    echo $Arya; // Notice: Undefined variable: Andi
+
+    // Statement
+    echo "<p>Hallo ges</p>";    
+    $a = 3; $b = 4;
+    $nama = "Informatika";
+    $c = $a / 25.0;
+    if ($a != $b) {
+        echo "<p>Pemrograman Web Praktikum</p>";
+    }
+
+    // Parse Error (Semicolon)
+    // $a = 5 + 7
+    // echo $a;
+?>
